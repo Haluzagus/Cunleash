45989e4d733915ee5e62c5448b583d066aa51d0e
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-13 15:23:13 +0300

    Update and rename TogglebleAutoGC11.js to TogglebleAutoGC12.js

--- TogglebleAutoGC12.js
+++ TogglebleAutoGC12.js
@@ -48,7 +48,7 @@
     }
   }, 1000);
 })();
-Game.Notify(`0,0,[0,0],false);
-Game.Notify(`1,0,[1,0],false);
-Game.Notify(`2,0,[2,0],false);
-Game.Notify(`3,0,[3,0],false);
+    Game.Notify(`0,0`,[0,0],false);
+Game.Notify(`1,0`,[1,0],false);
+Game.Notify(`2,0`,[2,0],false);
+Game.Notify(`3,0`,[3,0],false);
