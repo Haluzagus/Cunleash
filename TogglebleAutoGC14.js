141b56c5eeb33001dd47a5e7831a305e6191e854
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-13 15:24:57 +0300

    Update and rename TogglebleAutoGC13.js to TogglebleAutoGC14.js

--- TogglebleAutoGC14.js
+++ TogglebleAutoGC14.js
@@ -18,15 +18,14 @@
       }
     }
   });
-
-Game.Notify(`0,0`,[0,0],false);
-Game.Notify(`1,0`,[1,0],false);
-Game.Notify(`2,0`,[2,0],false);
-Game.Notify(`3,0`,[3,0],false);
   
   function startAutoGC() {
     console.log('Auto-GC started');
     Game.Notify(`Auto GC ON`,`Press Z to toggle`,[4,6],true);
+    Game.Notify(`0,0`,[0,0],false);
+Game.Notify(`1,0`,[1,0],false);
+Game.Notify(`2,0`,[2,0],false);
+Game.Notify(`3,0`,[3,0],false);
     intervalId = setInterval(function() {
     Game.shimmers.forEach(function(shimmer)
     {
