4bceae13b357433a00a6d49f4bcbc3ef006fc889
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-13 15:27:03 +0300

    Update and rename TogglebleAutoGC15.js to TogglebleAutoGC16.js

--- TogglebleAutoGC16.js
+++ TogglebleAutoGC16.js
@@ -19,10 +19,10 @@
     }
   });
 
-Game.Notify(`0,0`,``,[0,0],false);
-Game.Notify(`1,0`,``,[1,0],false);
-Game.Notify(`2,0`,``,[2,0],false);
-Game.Notify(`3,0`,``,[3,0],false);
+Game.Notify(`0,0`,``,[4,0],false);
+Game.Notify(`1,0`,``,[4,1],false);
+Game.Notify(`2,0`,``,[4,2],false);
+Game.Notify(`3,0`,``,[4,3],false);
   
   function startAutoGC() {
     console.log('Auto-GC started');
