2f1792225502579caae22bdbe2b23ecac79349da
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-13 14:48:10 +0300

    Update TogglebleAutoGC.js

--- TogglebleAutoGC.js
+++ TogglebleAutoGC.js
@@ -1,1 +1,52 @@
+(function() {
+  'use strict';
+
+  let state = true; // initial state is ON
+  let intervalId;
+
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyZ') {
+      state = !state; // toggle state
+      if (state) {
+        // code to run when state is ON
+        console.log('State is ON');
+        startAutoClicker();
+      } else {
+        // code to run when state is OFF
+        console.log('State is OFF');
+        stopAutoClicker();
+      }
+    }
+  });
+
+  
+  function startAutoGC() {
+    console.log('Auto-GC started');
+    Game.Notify(`Auto GC ON`,`Press Z to toggle`,[0,35],false);
+    intervalId = setInterval(function() {
+    Game.shimmers.forEach(function(shimmer)
+    {
+        if(shimmer.type == "golden" && shimmer.wrath == 0)
+        {
+            shimmer.pop()
+        }
+    })
+}, 500);
+  }
+
+  function stopAutoGC() {
+    console.log('Auto-GC stopped');
+    Game.Notify(`Auto GC OFF`,`Press Z to toggle`,[0,35],false);
+    clearInterval(intervalId);
+  }
+
+  // Wait for the Game object to be defined, then start the auto-clicker
+  const waitIntervalId = setInterval(function() {
+    if (typeof Game !== 'undefined') {
+      clearInterval(waitIntervalId);
+      startAutoGC();
+    }
+  }, 1000);
+})();
+
 
