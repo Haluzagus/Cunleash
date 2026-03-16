7563ff2c6e3448be9356377b8db88282a97dbd45
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-16 10:52:42 +0300

    Update and rename test10.js to test11.js

--- test11.js
+++ test11.js
@@ -1,3 +1,7 @@
 setTimeout(function() {
+  Game.LoadMod("https://klattmose.github.io/CookieClicker/CCSE.js");
+}, 200);
+
+setTimeout(function() {
   Game.LoadMod("https://cdn.jsdelivr.net/gh/Haluzagus/Cunleash@main/cookistocker26.js");
 }, 800);
