643d99d0062869434ce69197e9f502a430db76c1
Haluzagus <79799471+Haluzagus@users.noreply.github.com>
2026-03-17 13:25:13 +0300

    Delete test12.js

--- test12.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-  Game.LoadMod("https://klattmose.github.io/CookieClicker/CCSE.js");
-}, 200);
-
-setTimeout(function() {
-  Game.LoadMod("https://cdn.jsdelivr.net/gh/Haluzagus/Cunleash@main/cookistocker27.js");
-}, 800);
